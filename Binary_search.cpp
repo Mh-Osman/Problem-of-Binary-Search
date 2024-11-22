956978d664a2001281fee8c46fa6db13e0398ef9
Mh-Osman <osmangani1osm@gmail.com>
2024-11-22 20:57:46 +0600

    problem : Binary Search

---
+++ Binary_search.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+
+    
+    int search(vector<int>& nums, int target) {
+
+            int lo = 0;
+            int hi=nums.size()-1;
+            while(hi - lo >1){
+
+
+                int mid = (hi+lo)>>1;
+                if(nums[mid] < target){
+
+                    lo=mid+1;
+                }else{
+
+
+                    hi=mid;
+                }
+            }
+
+            if(nums[lo] == target){
+
+                return lo;
+            }else if(nums[hi] == target){
+
+                return hi;
+            }else{
+
+                return -1;
+            }
+        
+        
+    }
+};
